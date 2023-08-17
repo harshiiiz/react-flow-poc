9da4e27c4f6eafc141e3774e14542b32b942ddb5
Harshiiiz <harshita.sahu@roro.io>
2023-08-17 16:12:55 +0530

    Update GroupNode.js

--- src/shapes/GroupNode.js
+++ src/shapes/GroupNode.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Handle, Position } from 'reactflow';
 
 const GroupNode = () => {
   
@@ -7,6 +8,11 @@ const GroupNode = () => {
   
 
   return (
+    <>
+    <Handle type="source" position={Position.Top} id='1'/>
+    <Handle type="source" position={Position.Left} id='2'/>
+    <Handle type="source" position={Position.Right} id='3'/>
+    <Handle type="source" position={Position.Bottom} id='4'/>
     <div
       className="group-node"
      
@@ -14,6 +20,7 @@ const GroupNode = () => {
     >
       {/* ... (render child nodes) */}
     </div>
+    </>
   );
 };
 export default GroupNode;
